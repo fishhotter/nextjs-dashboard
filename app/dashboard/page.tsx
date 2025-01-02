5057b8a076c35289fab0b3f82598dad94e88ed29
caoxiang <caoxiang@admindeMacBook-Pro.local>
2025-01-02 20:56:33 +0800

    feat: 添加测试代码

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -24,7 +24,7 @@ export default async function Page() {
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Total Invoices1111" value={numberOfInvoices} type="invoices" />
         <Card
           title="Total Customers"
           value={numberOfCustomers}
